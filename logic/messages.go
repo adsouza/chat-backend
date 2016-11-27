a89f4ad8526ba788e854f948fca5d4f623bf34b2
Antonio D'souza <adsouza@gmail.com>
2016-11-27 18:56:35 -0500

    Add proto messages for video and image metadata and create it in the logic module when saving messages.

--- logic/messages.go
+++ logic/messages.go
@@ -32,18 +32,27 @@ func NewMessageController(db Db) *msgController {
 	return &msgController{db: db}
 }
 
-func (c *msgController) SendMessage(sender, recipient, message string) error {
-	metadata := &api.Metadata{}
-	url, err := url.Parse(message)
-	if err == nil {
-		if (url.Host == "www.youtube.com" || url.Host == "www.vevo.com") && strings.HasPrefix(url.Path, "/watch/") {
-			// We have a video!
+func metadataFromURL(url *url.URL) *api.Metadata {
+	if strings.HasPrefix(url.Path, "/watch/") {
+		switch url.Host {
+		case "www.youtube.com":
+			return &api.Metadata{Media: &api.Metadata_Video{Video: &api.Video{Source: api.Video_YOUTUBE}}}
+		case "www.vevo.com":
+			return &api.Metadata{Media: &api.Metadata_Video{Video: &api.Video{Source: api.Video_VEVO}}}
+		default:
 		}
-		// Assume we have an image.
 	}
-	data, err := proto.Marshal(metadata)
-	if err != nil {
-		return fmt.Errorf("could not marshal metadata proto into blob: %v", err)
+	// Assume we have an image.
+	return &api.Metadata{Media: &api.Metadata_Image{Image: &api.Image{}}}
+}
+
+func (c *msgController) SendMessage(sender, recipient, message string) error {
+	var data []byte
+	if url, err := url.Parse(message); err == nil {
+		data, err = proto.Marshal(metadataFromURL(url))
+		if err != nil {
+			return fmt.Errorf("could not marshal metadata proto into blob: %v", err)
+		}
 	}
 	return c.db.AddMessage(sender, recipient, message, data)
 }
